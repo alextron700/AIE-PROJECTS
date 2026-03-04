8a4e4740d6c262450786d933309e32b9b1a9be87
Alex <82306660+alextron700@users.noreply.github.com>
2026-03-04 13:42:22 +1100

    made basic programs

---
+++ Fizzbizz.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+std::string fizzbuzz(int input)
+{
+    bool F = false;
+    bool B = false;
+    std::string out = "";
+    if (input % 3 == 0)
+    {
+        out+= "fizz";
+        F = true;
+    }
+    if (input % 5 == 0)
+    {
+        out+= "buzz";
+        B = true;
+    }
+    if (input % 5 != 0 && input % 3 != 0)
+    {
+        out +=std::to_string(input);
+    }
+    return out;
+    
+}
+int main()
+{
+    int input = 7;
+    std::string FB = fizzbuzz(input);
+    if (input == 7)
+    {
+        assert(FB == "7");
+    }
+    input = 3;
+    FB = fizzbuzz(input);
+    if (input == 3)
+    {
+        assert(FB == "fizz");
+    }
+    input = 5;
+    FB = fizzbuzz(input);
+    if (input == 5)
+    {
+        assert(FB == "buzz");
+    }
+    input = 15;
+    FB = fizzbuzz(input);
+    if (input == 15)
+    {
+        assert(FB == "fizzbuzz");
+    }
+    return 77;
+}
